8e8384994b87f25e7055c081f48a5f6c5e67a29c
john681611 <john681611@hotmail.com>
2023-09-08 15:28:05 +0100

    Logic casts

---
+++ Payment.Logic/ObjectCasts.cs
@@ -0,0 +1,64 @@
+using Payment.CKOBankClient.Models;
+using Payment.DB.Models;
+using Payment.Logic.Models;
+
+namespace Payment.Logic;
+
+public static class ObjectCasts
+{
+    public static CKOPaymentRequest PaymentRequestToBankRequest(PaymentRequest paymentRequest)
+    {
+        return new CKOPaymentRequest
+        {
+            Ammount = paymentRequest.Ammount,
+            Currency = paymentRequest.Currency,
+            Referance = paymentRequest.Referance,
+            CardDetails = new CKOPaymentRequestCardDetails
+            {
+                Type = paymentRequest.CardDetails.Type,
+                Number = paymentRequest.CardDetails.Number,
+                CCV = paymentRequest.CardDetails.CCV,
+                Name = paymentRequest.CardDetails.Name,
+                Expiry = new CKOPaymentRequestExpiry
+                {
+                    Month = paymentRequest.CardDetails.Expiry.Month,
+                    Year = paymentRequest.CardDetails.Expiry.Year,
+                },
+            }
+        };
+    }
+
+    public static PaymentRecord PaymentRequestToRecord(PaymentRequest paymentRequest, CKOPaymentResponse bankResults)
+    {
+        return new PaymentRecord
+        {
+            PaymentId = bankResults.PaymentID,
+            MerchantID = paymentRequest.MerchantID,
+            Ammount = paymentRequest.Ammount,
+            Currency = paymentRequest.Currency,
+            Referance = paymentRequest.Referance,
+            CardType = paymentRequest.CardDetails.Type,
+            CardNumberFinalFour = paymentRequest.CardDetails.Number.Substring(paymentRequest.CardDetails.Number.Length - 4),
+            CardName = paymentRequest.CardDetails.Name,
+            ResultStatus = bankResults.StatusCode,
+            ResultText = bankResults.StatusText
+        };
+    }
+
+    public static PaymentDetailsResponse PaymentRecordToPaymentDetailsResponse(PaymentRecord paymentRecord)
+    {
+        return new PaymentDetailsResponse
+        {
+            PaymentId = paymentRecord.PaymentId,
+            MerchantID = paymentRecord.MerchantID,
+            Ammount = paymentRecord.Ammount,
+            Currency = paymentRecord.Currency,
+            Referance = paymentRecord.Referance,
+            CardNumberFinalFour = paymentRecord.CardNumberFinalFour,
+            CardName = paymentRecord.CardName,
+            CardType = paymentRecord.CardType,
+            ResultStatus = paymentRecord.ResultStatus,
+            ResultText = paymentRecord.ResultText,
+        };
+    }
+}
--- Payment.Logic/PaymentService.cs
+++ Payment.Logic/PaymentService.cs
@@ -1,8 +1,6 @@
-﻿using Payment.DB.Models;
-using Payment.DB.Services;
+﻿using Payment.DB.Services;
 using Payment.Logic.Models;
 using Payment.CKOBankClient;
-using Payment.CKOBankClient.Models;
 
 namespace Payment.Logic.Services;
 
@@ -19,8 +17,8 @@ public class PaymentService
     public async Task<PaymentResponse> MakePayment(PaymentRequest paymentRequest)
     {
         // TODO: VALIDATION
-        var bankResponse = await _CKOBankClient.MakePayment(PaymentRequestToBankRequest(paymentRequest));
-        await _paymentRecordService.CreateAsync(PaymentRequestToRecord(paymentRequest, bankResponse));
+        var bankResponse = await _CKOBankClient.MakePayment(ObjectCasts.PaymentRequestToBankRequest(paymentRequest));
+        await _paymentRecordService.CreateAsync(ObjectCasts.PaymentRequestToRecord(paymentRequest, bankResponse));
         return new PaymentResponse
         {
             PaymentID = "1234",
@@ -33,62 +31,6 @@ public class PaymentService
         var response = await _paymentRecordService.GetAsync(paymentDetailsRequest.PaymentID, paymentDetailsRequest.MerchantID);
         if (response == null)
             throw new KeyNotFoundException();
-        return PaymentRecordToPaymentDetailsResponse(response);
-    }
-
-    private CKOPaymentRequest PaymentRequestToBankRequest(PaymentRequest paymentRequest)
-    {
-        return new CKOPaymentRequest
-        {
-            Ammount = paymentRequest.Ammount,
-            Currency = paymentRequest.Currency,
-            Referance = paymentRequest.Referance,
-            CardDetails = new CKOPaymentRequestCardDetails
-            {
-                Type = paymentRequest.CardDetails.Type,
-                Number = paymentRequest.CardDetails.Number,
-                CCV = paymentRequest.CardDetails.CCV,
-                Name = paymentRequest.CardDetails.Name,
-                Expiry = new CKOPaymentRequestExpiry
-                {
-                    Month = paymentRequest.CardDetails.Expiry.Month,
-                    Year = paymentRequest.CardDetails.Expiry.Year,
-                },
-            }
-        };
-    }
-
-    private PaymentRecord PaymentRequestToRecord(PaymentRequest paymentRequest, CKOPaymentResponse bankResults)
-    {
-        return new PaymentRecord
-        {
-            PaymentId = bankResults.PaymentID,
-            MerchantID = paymentRequest.MerchantID,
-            Ammount = paymentRequest.Ammount,
-            Currency = paymentRequest.Currency,
-            Referance = paymentRequest.Referance,
-            CardType = paymentRequest.CardDetails.Type,
-            CardNumberFinalFour = paymentRequest.CardDetails.Number.Substring(paymentRequest.CardDetails.Number.Length - 4),
-            CardName = paymentRequest.CardDetails.Name,
-            ResultStatus = bankResults.StatusCode,
-            ResultText = bankResults.StatusText
-        };
-    }
-
-    private PaymentDetailsResponse PaymentRecordToPaymentDetailsResponse(PaymentRecord paymentRecord)
-    {
-        return new PaymentDetailsResponse
-        {
-            PaymentId = paymentRecord.PaymentId,
-            MerchantID = paymentRecord.MerchantID,
-            Ammount = paymentRecord.Ammount,
-            Currency = paymentRecord.Currency,
-            Referance = paymentRecord.Referance,
-            CardNumberFinalFour = paymentRecord.CardNumberFinalFour,
-            CardName = paymentRecord.CardName,
-            CardType = paymentRecord.CardType,
-            ResultStatus = paymentRecord.ResultStatus,
-            ResultText = paymentRecord.ResultText,
-        };
+        return ObjectCasts.PaymentRecordToPaymentDetailsResponse(response);
     }
 }
